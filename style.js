39ff5411955259fdd84663f4112579aaacfc12c6
Jordan-Marshh <119689575+Jordan-Marshh@users.noreply.github.com>
2023-04-20 09:56:27 -0600

    shirts n stuff 4/20

--- style.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollY;
-    var documentHeight = document.body.offsetHeight;
-    var windowHeight = window.innerHeight;
-    var percentageScrolled = (scrollPosition / (documentHeight - windowHeight)) * 100;
-    document.documentElement.style.setProperty('--scroll-percentage', percentageScrolled + '%');
-  });
